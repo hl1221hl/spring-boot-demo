dc9159cd0bb73d91ceb61df5247c70565da361a5
huanglei <huanglei@aiyong.com>
2021-06-16 00:44:53 +0800

    #Test

--- src/main/java/com/demo/controller/BaseController.java
+++ src/main/java/com/demo/controller/BaseController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * asdasd
+ */
 @Controller
 public class BaseController {
 
